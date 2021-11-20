9b3fed34d62281061454372aefb5948312c817a1
Lucas Ledesma <lucasledesma27@gmail.com>
2021-11-20 16:21:55 -0300

    correcion de bug

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,5 @@
 let Nombre = prompt("ingrese su nombre");
 let Numero = prompt("ingrese un numero");
 let Numero2 = 100;
-let Resultado = (Numero)+(Numero2);
+let Resultado = parseInt(Numero)+parseInt(Numero2);
 alert ("hola " + Nombre + " tu numero mas cien es " +Resultado);
