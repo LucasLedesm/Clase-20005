1ba7ddb2a411d08bf0232ae896f0ea8b9deed266
Lucas Ledesma <lucasledesma27@gmail.com>
2021-11-20 21:25:08 -0300

    desafio clase2

---
+++ clase2/Javascript.js
@@ -0,0 +1,20 @@
+let Nombre = prompt("saluda!");
+if ( Nombre == ("hola")){
+    alert("Muy bien!")
+}else {
+    alert("Saluda primero!")
+}
+let Numero = prompt("ingresa un numero");
+if ( Numero <= 1000) {
+ alert ("tu numero es menor a mil!")
+}else {
+    alert ("tu numero si es mas que mil")
+}
+let Numero2 = prompt("ingresa otro numero");
+if (Numero2 >= 10 & Numero2 <=50){
+    alert ("tu numero esta entre 10 y 50!")
+} else {
+    alert ("tu numero es mayor a 50")
+}
+// let Resultado = parseInt(Numero)+parseInt(Numero2);
+// alert ("hola " + Nombre + " tu numero mas cien es " +Resultado);
