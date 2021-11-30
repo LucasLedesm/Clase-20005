3ce9921e7eedf0b5e90dab2c72828fef4680d1cb
Lucas Ledesma <lucasledesma27@gmail.com>
2021-11-29 23:36:21 -0300

    correcion de version

--- clase3/Javascript.js
+++ clase3/Javascript.js
@@ -2,4 +2,4 @@ let entrada = parseInt( prompt("Ingresa un numero"));
 while( entrada > 0) {
     alert("el numero que le sigue es: " + (entrada+1));
     entrada = parseInt (prompt("ingresa otro numero o 0 para cancelar"))
-}
+};
