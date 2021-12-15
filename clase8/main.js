a212b5affe0de3a920bfb603154829660aa0e856
Lucas Ledesma <lucasledesma27@gmail.com>
2021-12-15 00:41:46 -0300

    correciones

--- clase8/main.js
+++ clase8/main.js
@@ -3,9 +3,10 @@ const addNewPacient = ()=> {
     let pacientName = document.getElementById("inputName").value;
     let pacientLastName = document.getElementById("inputLastName").value;
     let pacientVacuna = document.getElementById("inputVacuna").value;
+    let pacientDosis = document.getElementById("inputDosis").value;
     let newPacient = document.createElement("div");
     newPacient.className = 'col-md-12';
-    newPacient.innerHTML = `<h2>${pacientName} ${pacientLastName}</h2><p>vacuna: ${pacientVacuna}</p>`;
+    newPacient.innerHTML = `<h2>${pacientName} ${pacientLastName}</h2><p>vacuna: ${pacientVacuna}</p><p>dosis: ${pacientDosis}</p>`;
     getPacientList.appendChild(newPacient);
     console.log(Pacient)
 };
