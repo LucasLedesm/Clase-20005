aaf0e419dd55e388cd6f6db0a8d7595b55f460c5
Lucas Ledesma <lucasledesma27@gmail.com>
2021-12-18 19:52:44 -0300

    se corrigio bug

--- clase8/main.js
+++ clase8/main.js
@@ -1,4 +1,4 @@
-const addNewPacient = ()=> {
+const addNewPacient = () => {
     let getPacientList = document.getElementById("Pacient");
     let pacientName = document.getElementById("inputName").value;
     let pacientLastName = document.getElementById("inputLastName").value;
@@ -6,11 +6,13 @@ const addNewPacient = ()=> {
     let pacientDosis = document.getElementById("inputDosis").value;
     let newPacient = document.createElement("div");
     newPacient.className = 'col-md-12';
-    newPacient.innerHTML = `<h2>${pacientName} ${pacientLastName}</h2><p>vacuna: ${pacientVacuna}</p><p>dosis: ${pacientDosis}</p>`;
+    newPacient.innerHTML = `<div id = clienteHijos><h2>${pacientName} ${pacientLastName}</h2><p>vacuna: ${pacientVacuna}</p><p>dosis: ${pacientDosis}</p></div>`;
     getPacientList.appendChild(newPacient);
     console.log(Pacient)
 };
+
+
 const clearList = ()=> {
-    let clear =document.getElementById("Pacient");
-    clear.parentNode.removeChild(Pacient)
+    let hijo =document.getElementById("clienteHijos");
+    hijo.remove()
 };
