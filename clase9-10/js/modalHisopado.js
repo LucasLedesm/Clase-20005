69915df68466914bbdd3fa5a6325de13b9950282
Lucas Ledesma <lucasledesma27@gmail.com>
2022-01-20 21:52:13 -0300

    se agrego GET a hisopados

--- clase9-10/js/modalHisopado.js
+++ clase9-10/js/modalHisopado.js
@@ -20,3 +20,26 @@ let hoy = new Date();
 let fecha = hoy.getDate() + '-' + (hoy.getMonth() + 1) + '-' + hoy.getFullYear();
 let hora = hoy.getHours() + ':' + hoy.getMinutes() + ':' + hoy.getSeconds();
 let fechaYHora = fecha + ' ' + hora;
+
+
+let url = "../pacientesHisopados.json"
+$.get(url,function(response, status){
+    console.log(response)
+    console.log(status);
+    let html= ""; 
+    if (status === "success"){
+        for(const dato of response){
+
+            html +=
+             `<ul> <li>Nombre:${dato.name}</li>
+             <li>Apellido: ${dato.lastName}</li>
+             <li>1er sintoma: ${dato.sintoma}</li>
+             <li>2do sintoma: ${dato.sintomatwo}</li>
+              </ul>
+              <br> `
+        
+        }
+        $("#appH").html(html);
+    }
+})
+
